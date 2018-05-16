73e596d615f397a40877f3cb1f395bd9fe2afe4a
JackManson2017 <33890294+SonHOuse1993@users.noreply.github.com>
2018-05-16 11:55:05 +0800

    11

--- Ch04/程序清单4-3.py
+++ Ch04/程序清单4-3.py
@@ -2,7 +2,6 @@
 
 from numpy import *
 
-
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
